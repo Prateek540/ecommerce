1ff197071e70fc37618644cda06623df4cfc322f
Prateek <prateekpathak97@gmail.com>
2023-03-11 19:32:08 +0530

    Update it up!!!

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
           <Route path="/electronics" element={<Electronics />} />
           <Route path="/fashion" element={<Fashion />} />
         </Routes>
